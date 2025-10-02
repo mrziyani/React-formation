6623c3b6ffd041e28990a92b6dbd6df5feb83e4a
Reda Ziyani <reda.ziyani@bobst.com>
2025-10-02 15:06:16 +0100

    naviation learn

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import MyButton from './MyButton';
 import About from './About';
+import Login from './Login';
 
 function App() {
-  function handleClick(){
-        setCount(count+1)
-        alert(`clicked ${count+1} times`);
-    }
-    const [count, setCount] = useState(0);  
+  const [count, setCount] = useState(0);
+
+  function handleClickTest() {
+    setCount(count + 1);
+    alert(`clicked ${count + 1} times`);
+  }
+
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home count={count} handleClickTest={handleClickTest} />}
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+
+function Home({ count, handleClickTest }) {
+  const navigate = useNavigate();
+
   return (
     <>
-      <h1>reda ziyani</h1>
-      <MyButton count={count} onClick={handleClick} ></MyButton>
-      <MyButton count={count} onClick={handleClick} ></MyButton>
-      <About></About>
+      <h1>reda ziyani Quick start</h1>
+      <MyButton count={count} onClick={handleClickTest}></MyButton>
+      <MyButton count={count} onClick={handleClickTest}></MyButton>
+      <About />
+      <button onClick={() => navigate('/login')}>
+        Go to Login to test login
+      </button>
     </>
   );
 }
 
 export default App;
-
